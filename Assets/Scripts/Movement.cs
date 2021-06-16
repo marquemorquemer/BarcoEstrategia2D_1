8e22ba39a9ff3d6652633b3cac06f2dac43ce0c4
marquemorquemer <m.solssilva@uandresbello.edu>
2021-06-15 21:53:08 -0400

    Mov jp, enemy mov, selector change

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 5;
+    public Transform transformAMover;
+    public Vector2 directionShoot;
+    public bool isUsingMouse = true;
+    public Rigidbody2D rb;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector2(x, y) * speed * Time.fixedDeltaTime;
+
+        //transformAMover.Translate(transformAMover.right * x * speed * Time.deltaTime);
+        //transformAMover.Translate(transformAMover.up * y * speed * Time.deltaTime);
+
+        float directionX = (Input.GetAxisRaw("Horizontal"));
+        float directionY = (Input.GetAxisRaw("Vertical"));
+        if (directionX != 0 || directionY != 0)
+        {
+            directionShoot = (new Vector2(directionX, directionY)).normalized;
+        }
+        if (isUsingMouse)
+        {
+            float mousePosX = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
+            float mousePosY = Camera.main.ScreenToWorldPoint(Input.mousePosition).y;
+            if (mousePosX != 0 || mousePosY != 0)
+            {
+                directionShoot = (new Vector2(mousePosX, mousePosY) - (Vector2)transform.position).normalized;
+            }
+        }
+        //else
+        //{
+        //    float mousePosX = 10 * Input.GetAxis("XboxSecondAnalogHorizontal");
+        //    float mousePosY = 10 * Input.GetAxis("XboxSecondAnalogVertical");
+        //    if (mousePosX != 0 || mousePosY != 0)
+        //    {
+        //        directionShoot = (new Vector2(mousePosX, mousePosY) - (Vector2)transform.position).normalized;
+        //    }
+        //}
+    }
+}
---
+++ Assets/Scripts/SaleccionDeBarco.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaleccionDeBarco : MonoBehaviour
+{
+    public Movement movBarco;
+    public Rigidbody2D rb2D;
+
+    public float speedBarco = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Barco Seleccionado, {0}" + this.name);
+
+        movBarco.rb = rb2D;
+
+        movBarco.speed = speedBarco;
+    }
+
+}
